cbec68f3be182556e9cf6a006c62cb253d4053d2
905002867@qq.com <EY666662015>
2020-12-21 20:21:14 +0800

    test

---
+++ jundunvues/src/components/goods_guanli/AssetsOverview.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="assetsoverview">
+    
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+
+      }
+    },
+    methods:{
+
+    },
+
+    /* created() {
+
+    }, */
+
+  }
+</script>
+
+<style>
+</style>
+<style scoped="scoped">
+  #assetsoverview{
+    
+  }
+ </style>
