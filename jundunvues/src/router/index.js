e9b7f5f9c9e8523b282866c0173967c5b9ce36f8
pengxinyuu@163.com <pengxinyuu@163.com>
2020-12-21 21:10:06 +0800

    我是导航栏

--- jundunvues/src/router/index.js
+++ jundunvues/src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import page from '@/components/page'
+import pxyemp from '@/components/PxyEmp'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/page',
+      name: 'page',
+      component: page
+    },
+    {
+      path: '/pxyemp',
+      name: 'pxyemp',
+      component: pxyemp
     }
+
   ]
 })
